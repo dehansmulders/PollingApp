df78c5981b19adc7adf425e96bf4bfe2ad1f4012
Dehan Smulders <dehans@jurumani.com>
2021-03-20 20:52:53 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PollingAssignment
 A basic application where users create a question poll, submit an answer and view results
+
+## To run the app
+
+Navigate to the 'poller' folder.
+From the root directory, this should simply be:
+
+### 'cd poller'
+
+then just run the app with:
+
+### 'npm start'
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
--- poller/src/App.test.js
+++ poller/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { TextField } from "@material-ui/core"
 
-test('renders learn react link', () => {
+test('the app has rendered', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  screen.getByText("Poll Creator 2000");
+});
+
+test('user can add a new option to the list', () => {
+  const onChange = jest.spyOn(TextField.prototype, 'onChange');
+  const inputField = screen.getByText(/Type an answer:/i);
+  const addButton = screen.getByText("Add");
+  
+  fireEvent.change(inputField, {target: {value: "TestOption"}});
+  fireEvent.click(addButton)
+
+  screen.getByText("TestOption")
 });
